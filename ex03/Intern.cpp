52fba757fd2986112541e69754d138435c97248c
Artabarean <artabarean@gmail.com>
2026-05-26 08:53:24 +0000

    ex03

--- ex03/Intern.cpp
+++ ex03/Intern.cpp
@@ -20,11 +20,20 @@ Intern& Intern::operator=(const Intern&)
 AForm *Intern::makeForm(const std::string &formName, const std::string &target)
 {
     if (formName == "shrubbery creation")
+    {
+        std::cout << "Intern creates " << formName << " form." << std::endl;
         return (new ShrubberyCreationForm(target));
+    }
     else if (formName == "robotomy request")
+    {
+        std::cout << "Intern creates " << formName << " form." << std::endl;
         return (new RobotomyRequestForm(target));
+    }
     else if (formName == "presidential pardon")
+    {
+        std::cout << "Intern creates " << formName << " form." << std::endl;
         return (new PresidentialPardonForm(target));
+    }
     else
     {
         std::cout << "Intern couldn't create form: " << formName << std::endl;
